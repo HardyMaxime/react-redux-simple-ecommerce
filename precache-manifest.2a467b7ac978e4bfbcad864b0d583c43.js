3665213ddd5a73165a85d16f1716e70689f7f519
Maxime <hardymaxime7@gmail.com>
2019-01-08 17:39:08 +0100

    Updates

---
+++ precache-manifest.2a467b7ac978e4bfbcad864b0d583c43.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-redux-simple-ecommerce/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "e0f7ef773590f8719c71",
+    "url": "/react-redux-simple-ecommerce/static/css/main.08aef194.chunk.css"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react-redux-simple-ecommerce/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "117552379ce1311bf5e8",
+    "url": "/react-redux-simple-ecommerce/static/js/1.11755237.chunk.js"
+  },
+  {
+    "revision": "96fc9342a59db5052d85",
+    "url": "/react-redux-simple-ecommerce/static/js/runtime~main.96fc9342.js"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react-redux-simple-ecommerce/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-redux-simple-ecommerce/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-redux-simple-ecommerce/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-redux-simple-ecommerce/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-redux-simple-ecommerce/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react-redux-simple-ecommerce/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "e0f7ef773590f8719c71",
+    "url": "/react-redux-simple-ecommerce/static/js/main.e0f7ef77.chunk.js"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-redux-simple-ecommerce/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-redux-simple-ecommerce/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-redux-simple-ecommerce/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-redux-simple-ecommerce/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react-redux-simple-ecommerce/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-redux-simple-ecommerce/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react-redux-simple-ecommerce/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-redux-simple-ecommerce/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "117552379ce1311bf5e8",
+    "url": "/react-redux-simple-ecommerce/static/css/1.19f0267d.chunk.css"
+  },
+  {
+    "revision": "645c00fcde0830f84e3cfedb18f8cd4b",
+    "url": "/react-redux-simple-ecommerce/index.html"
+  }
+];
